d39737ebf8e13b8fdba8dcb1e0202f75515c1c98
Jonas Nicklas and Kim Burgestrand <dev+jnicklas+burgestrand@elabs.se>
2014-11-06 13:46:27 +0100

    Better caching behaviour

--- index.js
+++ index.js
@@ -1,32 +1,56 @@
 'use strict';
+
 var RSVP = require('rsvp');
 var webpack = require('webpack');
-var CachingWriter = require('broccoli-caching-writer');
+var syncDir = require("./sync_dir");
+var path = require("path");
+var fs = require("fs.extra");
 
 function WebpackWriter(inputTree, options) {
 	if (!(this instanceof WebpackWriter)) return new WebpackWriter(inputTree, options);
 	this.inputTree = inputTree;
 	this.options = options;
+  this.inPath = path.join(process.cwd(), "./tmp/webpack-in");
+  this.outPath = path.join(process.cwd(), "./tmp/webpack-out");
 }
-WebpackWriter.prototype = Object.create(CachingWriter.prototype);
-WebpackWriter.prototype.constructor = WebpackWriter;
-WebpackWriter.prototype.updateCache = function(srcDir, destDir) {
-	this.options['context'] = srcDir;
-	this.options['output'] = this.options['output'] || {};
-	this.options['output']['path'] = destDir;
-	var compiler = webpack(this.options);
-	return new RSVP.Promise(function(resolve, reject) {
-		compiler.run(function(err, stats) {
-			var jsonStats = stats.toJson();
-			if (jsonStats.errors.length > 0) jsonStats.errors.forEach(console.error);
-			if (jsonStats.warnings.length > 0) jsonStats.warnings.forEach(console.warn);
-			if (err || jsonStats.errors.length > 0) {
-				reject(err);
-			} else {
-				resolve(destDir);
-			}
-		});
-	});
+
+WebpackWriter.prototype.read = function(readTree) {
+  var that = this
+
+  return readTree(this.inputTree).then(function(tmpPath) {
+    syncDir(tmpPath, that.inPath);
+
+    var compiler;
+
+    that.options['context'] = that.inPath;
+    that.options['output'] = that.options['output'] || {};
+    that.options['output']['path'] = that.outPath;
+
+    if (that.compiler) {
+      compiler = that.compiler;
+    } else {
+      compiler = webpack(that.options);
+      that.compiler = compiler;
+    }
+
+    return new RSVP.Promise(function(resolve, reject) {
+      compiler.run(function(err, stats) {
+        var jsonStats = stats.toJson();
+        if (jsonStats.errors.length > 0) jsonStats.errors.forEach(console.error);
+        if (jsonStats.warnings.length > 0) jsonStats.warnings.forEach(console.warn);
+        if (err || jsonStats.errors.length > 0) {
+          reject(err);
+        } else {
+          resolve(that.outPath);
+        }
+      });
+    });
+  });
+}
+
+WebpackWriter.prototype.cleanup = function() {
+  fs.rmrfSync(this.inPath);
+  fs.rmrfSync(this.outPath);
 }
 
 module.exports = WebpackWriter;
---
+++ sync_dir.js
@@ -0,0 +1,57 @@
+var fs = require("fs.extra");
+var path = require("path");
+var XXHash = require("xxhash");
+var seed = 0xCAFEBABE;
+var empty = {};
+
+function copySync(src, dest) {
+  var contents = fs.readFileSync(src)
+  fs.writeFileSync(dest, contents, { flag: 'wx' })
+}
+
+function hash(src) {
+  var contents = fs.readFileSync(src);
+  if(contents.length) {
+    return XXHash.hash(contents, seed);
+  } else {
+    return empty;
+  }
+}
+
+function syncDir (src, dest) {
+  var srcStats = fs.statSync(src);
+
+  try {
+    var destStats = fs.statSync(dest);
+  } catch(e) {
+  }
+
+  if (srcStats.isDirectory()) {
+    if(destStats && !destStats.isDirectory()) {
+      fs.unlinkSync(dest);
+    } else if(!destStats) {
+      fs.mkdirSync(dest);
+    }
+
+    var entries = fs.readdirSync(src).sort()
+    for (var i = 0; i < entries.length; i++) {
+      syncDir(src + path.sep + entries[i], dest + path.sep + entries[i])
+    }
+  } else if (srcStats.isFile()) {
+    if(destStats && destStats.isDirectory()) {
+      fs.rmrfSync(dest);
+      copySync(src, dest);
+    } else if(!destStats) {
+      copySync(src, dest);
+    } else {
+      if (hash(src) !== hash(dest)) {
+        fs.unlinkSync(dest);
+        copySync(src, dest);
+      }
+    }
+  } else {
+    throw new Error('Unexpected file type for ' + src)
+  }
+}
+
+module.exports = syncDir;
